2a3fcded5fddef87a28c93679d002b09ce24dc14
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-08-08 02:22:41 +0300

    Add exceptions

---
+++ wb_api/exceptions.py
@@ -0,0 +1,22 @@
+class TokenIsMissing(Exception):
+    pass
+
+
+class InvalidToken(Exception):
+    pass
+
+
+class TokenIsMalformed(Exception):
+    pass
+
+
+class TokenNotFound(Exception):
+    pass
+
+
+class TokenIsNotApplicable(Exception):
+    pass
+
+
+class ToManyRequests(Exception):
+    pass
