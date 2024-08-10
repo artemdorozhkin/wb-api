51486269b1b0ea14e507b38f07727163182f6728
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-08-11 00:17:39 +0300

    Change lib information

--- wb_api/__init__.py
+++ wb_api/__init__.py
@@ -1,5 +1,6 @@
 from wb_api.client import WBApi
 
-__author__ = "https://github.com/artemdorozhkin"
+__author__ = "artemdorozhkin"
+__url__ = "https://github.com/artemdorozhkin"
 __email__ = "aa.dorozhkin@ya.ru"
 __version__ = "0.1.0"
