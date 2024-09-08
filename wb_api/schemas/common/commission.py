76c7ec0c6ab2c2f79e1e8a4ea325005a7959d061
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-09-09 01:18:05 +0300

    Add description for Commission attributes

--- wb_api/schemas/common/commission.py
+++ wb_api/schemas/common/commission.py
@@ -8,24 +8,42 @@ class Commission(BaseModel):
     Модель комиссии по категориям товаров.
 
     Attributes:
-        kgvp_marketplace (float)
-        kgvp_supplier (float)
-        kgvp_supplier_express (float)
-        paid_storage_kgvp (float)
-        parent_ID (int)
-        parent_name (str)
-        subject_ID (int)
-        subject_name (str)
+        kgvp_marketplace (float): Комиссия по модели Склад продавца — везу на склад WB, %
+        kgvp_supplier (float): Комиссия по моделям Склад продавца — везу самостоятельно до клиента и Склад продавца — Курьером WB, %
+        kgvp_supplier_express (float): Комиссия по модели Склад продавца — везу самостоятельно до клиента экспресс, %
+        paid_storage_kgvp (float): Комиссия по модели Склад WB, %
+        parent_ID (int): ID родительской категории
+        parent_name (str): Название родительской категории
+        subject_ID (int): ID предмета
+        subject_name (str): Название предмета
     """
 
-    kgvp_marketplace: float = Field(..., alias="kgvpMarketplace")
-    kgvp_supplier: float = Field(..., alias="kgvpSupplier")
-    kgvp_supplier_express: float = Field(..., alias="kgvpSupplierExpress")
-    paid_storage_kgvp: float = Field(..., alias="paidStorageKgvp")
-    parent_ID: int = Field(..., alias="parentID")
-    parent_name: str = Field(..., alias="parentName")
-    subject_ID: int = Field(..., alias="subjectID")
-    subject_name: str = Field(..., alias="subjectName")
+    kgvp_marketplace: float = Field(
+        ...,
+        alias="kgvpMarketplace",
+        description="Комиссия по модели Склад продавца — везу на склад WB, %",
+    )
+    kgvp_supplier: float = Field(
+        ...,
+        alias="kgvpSupplier",
+        description="Комиссия по моделям Склад продавца — везу самостоятельно до клиента и Склад продавца — Курьером WB, %",
+    )
+    kgvp_supplier_express: float = Field(
+        ...,
+        alias="kgvpSupplierExpress",
+        description="Комиссия по модели Склад продавца — везу самостоятельно до клиента экспресс, %",
+    )
+    paid_storage_kgvp: float = Field(
+        ..., alias="paidStorageKgvp", description="Комиссия по модели Склад WB, %"
+    )
+    parent_ID: int = Field(
+        ..., alias="parentID", description="ID родительской категории"
+    )
+    parent_name: str = Field(
+        ..., alias="parentName", description="Название родительской категории"
+    )
+    subject_ID: int = Field(..., alias="subjectID", description="ID предмета")
+    subject_name: str = Field(..., alias="subjectName", description="Название предмета")
 
     class Config:
         populate_by_name = True
