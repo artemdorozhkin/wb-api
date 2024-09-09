4f020d281e664db337f1978cef43a25eb7e91f58
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-09-09 12:19:36 +0300

    Up version to 0.3.0

--- wb_api/__init__.py
+++ wb_api/__init__.py
@@ -3,4 +3,4 @@ from wb_api.client import WBApi
 __author__ = "artemdorozhkin"
 __url__ = "https://github.com/artemdorozhkin/wb-api"
 __email__ = "aa.dorozhkin@ya.ru"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
